e6e2d05147895a145e2233806cc3a1000fa98819
somayya <somayya.adusumalli98@gmail.com>
2025-10-30 20:40:48 +0530

    terraform

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -13,13 +13,35 @@ resource "aws_instance" "terraform" {
       on_failure = continue
     }
 
-    provisioner "local-exec"{
-      command = "echo Instance is destroyed"
-      when = destroy
-    }
-    # provisioner "local-exec" {
-    #   command = "${var.is_windows ? "cmd /C echo Instance is destroyed" : "echo Instance is destroyed"}"
+    # provisioner "local-exec"{
+    #   command = "echo Instance is destroyed"
+    #   when = destroy
     # }
+    provisioner "local-exec" {
+      command = "${var.is_windows ? "cmd /C echo Instance is destroyed" : "echo Instance is destroyed"}"
+    }
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      password    = "DevOps321"
+      host        = self.public_ip
+  }
+
+    provisioner "remote-exec" {
+      inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+    
+    provisioner "remote-exec" {
+      inline = [
+      "sudo systemctl start nginx",
+      "echo 'sucessfully stopped nginx server' "
+    ]
+    when = destroy
+  }
 
 }
 
